f69499392e65cd87b4d31d05c1616f49218b00cf
Daniel Obegi <your-email@example.com>
2026-02-04 04:01:27 -0800

    Seventh commit

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ title, styles  }) => {
+  return (
+    <div className={` flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full ${styles}`}>
+      <p className=" text-sm ">{title}</p>
+    </div>
+  )
+}
+
+export default Button;
